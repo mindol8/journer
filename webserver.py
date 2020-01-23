7a8f23de2a37503b21ba3983dc0ac9cc67d9ab1b
mindol8 <dlalstjr333@naver.com>
2020-01-23 20:32:01 +0900

    ui

--- webserver.py
+++ webserver.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def main_page():
     return render_template('main.html')
 
+
+@app.route('/signup',methods=['POST'])
+def sign_up():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()
