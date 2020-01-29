78c4e8374a0e4b99c9ad632414d68da19c0f8b15
mindol8 <dlalstjr333@naver.com>
2020-01-29 17:24:22 +0900

    signin page

--- webserver.py
+++ webserver.py
@@ -6,10 +6,26 @@ app = Flask(__name__)
 def main_page():
     return render_template('main.html')
 
+"""
+    signin page
+"""
+@app.route('/signin',methods=['GET'])
+def sign_in():
+    return render_template('signin.html')
 
-@app.route('/signup',methods=['POST'])
-def sign_up():
-    return render_template('signup.html')
+"""
+    login information
+"""
+@app.route('/main',methods = ["POST"])
+def login():
+    id = request.form['id']
+    pw = request.form['pw']
+   # print(id)
+   # print(pw)
+    """
+        로그인 끝나면 다시 메인 화면으로 이동
+    """
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
