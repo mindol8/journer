8cdcc27a9b0b04b40c664a05614a94b62ec98a52
ko6154 <45277136+ko6154@users.noreply.github.com>
2020-02-05 09:33:50 +0900

    DB 기본 연결 방법 설정

--- DAL.py
+++ DAL.py
@@ -12,26 +12,48 @@ ACCOUNT_TABLE_NAME : TEST_ACCOUNT
 ID VARCHAR2(20)
 PASSWORD VARCHAR2(20)
 '''
-
 import cx_Oracle
 
 def db_connection(account = 'journer', password = 'traveler', ip = '127.0.0.1:1521', sid = 'orcl'):
+    '''
+    :param account: Database account
+    :param password: Database password
+    :param ip: Database server ip address (format - ip:port)
+    :param sid: Database system identifier
+    :return: Connection , Cursor of Connected Database
+    '''
 
     con = cx_Oracle.connect(f'{account}/{password}@{ip}/{sid}')
     cur = con.cursor()
 
     return con , cur
 
+
+
 #default 값으로 연결
-#a , b = db_connection()
-
-#인자 지정 연결
-connection, cursor = db_connection('journer','traveler')
+#connection, cursor = db_connection()
 
 
-cursor.execute('SELECT * FROM TEST_ACCOUNT')
+try:
+    # 인자 지정 연결
+    connection, cursor = db_connection('journer','traveler')
+    cursor.execute('SELECT * FROM TEST_ACCOUNT')
+
+    for result in cursor:
+        print(result)
+except:
+    print('DB Connection failed')
+finally:
+    # 사용후 close
+    cursor.close()
+    connection.commit()
+    connection.close()
 
 
-for result in cursor:
-    print(result)
+#아래와 같은 방법으로도 DB 사용 가능
+# sql = "INSERT INTO TEST_ACCOUNT VALUES(:1,:2)"
+# data = ('TEST003','4123')
+#
+# cursor.execute(sql,data)
+
 
--- webserver.py
+++ webserver.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from DAL import *
 
 app = Flask(__name__)
 
