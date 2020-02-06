55e075c8d570d0c25ea4521bed488bbf49b281de
idontknowusername <fiveshho@gmail.com>
2020-02-06 12:47:57 +0900

    로그인 성공, 실패 액션

--- webserver.py
+++ webserver.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-#from DAL import *
+from flask import Flask, render_template, request, redirect
+from DAL import *
 
 app = Flask(__name__)
 
@@ -36,12 +36,12 @@ def find():
 """login information"""
 
 
-@app.route('/main', methods = ["POST"])
+@app.route('/login', methods=["POST"])
 def login():
     email = request.form['id']
     pw = request.form['pw']
 
-    login_query  = "SELECT COUNT(*) FROM ACCOUNT "
+    login_query = "SELECT COUNT(*) FROM ACCOUNT "
     login_query += "WHERE 1 = 1 "
     login_query += f"AND EMAIL = '{email}' "
     login_query += f"AND PASSWORD = '{pw}'"
@@ -59,8 +59,12 @@ def login():
         for row in cursor:
             if row[0] == 1:
                 print('login 성공!')
+                # 임시로 로그인 성공시 index 페이지로 보내줍니다. 추후 수정이 필요합니다.
+                return redirect('/')
             else:
                 print('이메일 또는 비밀번호를 다시 확인해 주십시오.')
+                # 로그인 실패시 signin 페이지를 다시 렌더링 해주고, fail 파라미터를 보내줘서 문구를 띄워줍니다.
+                return render_template('signin.html', fail=True)
 
     except:
         print('이메일 또는 비밀번호를 다시 확인해 주십시오.')
